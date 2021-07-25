4d8af09245d700ef41d29dcc463a9db055a2e44f
palakmalik5 <palakmalik5pm@gmail.com>
2021-07-24 18:02:21 -0700

    weather css and js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="Weather">
-        <h1> Weather App</h1>
-        < WeatherSearch />
-        <a href="github.com/weather">Source by Palak Malik</a>
+      <h1> Weather App</h1>
+      <WeatherSearch />
+      <a href="https://github.com/palakmalik5/weather">Source by Palak Malik</a>
     </div>
   );
 }
